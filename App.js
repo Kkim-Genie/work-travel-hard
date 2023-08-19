0cad3e83839da356eaed43e2608666fdcfca62aa
Kkim-Genie <kimgenie0409@gmail.com>
2023-08-19 03:15:29 +0000

    [Update] Header UI

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from'react-native';
+import { theme } from "./colors";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [working, setWorking] = useState(true);
+    const [text, setText] = useState("");
+    const travel = () => setWorking(false);
+    const work = () => setWorking(true);
+    const onChangeText = (event) => setText(event);
+    return (
+        <View style={styles.container}>
+            <StatusBar style="light" />
+            <View style={styles.header}>
+                <TouchableOpacity onPress={work}>
+                    <Text style={{...styles.btnText, color: working ? "white" : theme.grey}}>Work</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={travel}>
+                    <Text style={{...styles.btnText, color: !working ? "white" : theme.grey}}>Travel</Text>
+                </TouchableOpacity>
+            </View>
+            <TextInput 
+                placeholder={working ? "Add a To Do" : "Where do you want to go?"} 
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={text}
+                />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: theme.bg,
+        paddingHorizontal: 20,
+    },
+    header: {
+        justifyContent:'space-between',
+        flexDirection: 'row',
+        marginTop: 100
+    },
+    btnText: {
+        fontSize: 38,
+        fontWeight: 600,
+        color: "white"
+    },
+    input: {
+        backgroundColor: "white",
+        paddingVertical: 15,
+        paddingHorizontal: 20,
+        borderRadius: 30,
+        marginTop: 20,
+        fontSize: 18,
+    }
 });
